446bb7cd5a044e84463fc2480ab482a89408b87e
MikeTheFyke <mikefyke@hotmail.com>
2020-09-25 22:12:11 -0400

    3rd git variables

--- Program.cs
+++ Program.cs
@@ -5,9 +5,24 @@ namespace csharp
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+        {            
+            // Initial Git
+            // Console.WriteLine("Hello World!");
+            // Console.ReadLine();
+
+            // Second Git
+            // Console.Title = "Mike Title";
+            // Console.ForegroundColor = ConsoleColor.Red;
+            // Console.WindowHeight = 40;
+            // Console.WriteLine("Hello, whats your name ?");
+            // Console.ReadLine();
+            // Console.WriteLine("Nice to meet you.\nMy name is Hal,\n do you like daisies ?");
+            // Console.ReadLine();
+            // Console.ReadKey();
+
+            // Third Git
+            Console.WriteLine("Heloo what is your name ?");
+            string name = "";
         }
     }
 }
