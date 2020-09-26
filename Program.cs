db2c55fc7d1e758bfd230e193e32cc6f3fad93ff
MikeTheFyke <mikefyke@hotmail.com>
2020-09-26 19:51:40 -0400

    Arrays with set number of indices

--- Program.cs
+++ Program.cs
@@ -209,6 +209,28 @@ namespace csharp
             //     Console.WriteLine(rank + ". " + movies[i]);
             // };
 
+            // Thirteenth Git
+
+            // string[] movies = new string [4];
+
+            // Console.WriteLine("Please enter your four favorite movies : ");
+            
+            // for (var i = 0; i < movies.Length; i++){
+            //     movies[i] = Console.ReadLine();
+            // }
+            // movies[0] = Console.ReadLine();
+            // movies[1] = Console.ReadLine();
+            // movies[2] = Console.ReadLine();
+            // movies[3] = Console.ReadLine();
+
+            // Console.WriteLine("\nHere they are alphabetically : ");
+
+            // Array.Sort(movies); // Sorts array Alphabetically.
+
+            // for (var i  = 0; i < movies.Length; i++){
+            //     Console.WriteLine(movies[i]);
+            // };
+
             Console.ReadKey(); // waits for a key input before closing.
         }
     }
